c4de22071e132c19a4a7da2803974e86882d1c2e
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2025-04-03 01:12:15 +0530

    Seperate function for reading the count variable can be omitted as changing the visibility of the count varible to public will give us a function by default to read the varible

--- Counter.sol
+++ Counter.sol
@@ -2,16 +2,9 @@ pragma solidity ^0.8.0;
 
 contract MyContract {
 
-    uint count;
+    uint public count=0;
 
-    constructor() public {
-        count=0;
-    }
 
-//Function to read the count
-    function getCount() public view returns(uint){
-        return count;
-    }
 // Function to write the count
     function incrementCount() public {
         count=count+1;
