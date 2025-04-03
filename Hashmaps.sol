971cbfe3b2f274a636ffcd445b91f8a6307ce93e
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2025-04-03 16:17:12 +0530

    Mappings can be created using user defined data types such as Book here

--- Hashmaps.sol
+++ Hashmaps.sol
@@ -1,7 +1,16 @@
+// SPDX-License-Identifier:MIT
+
 pragma solidity ^0.8.0;
 
 contract HashMaps{
-    mapping(uint => string) public names;
+    mapping(uint => string) public names; // simple id->name mapping
+    mapping(uint=>Book) public Books; // Book is a struct type , mapping id to the Book type
+
+   struct Book{
+    string title;
+    string author;
+   }
+
     
     constructor(){
         names[1]="Anil";
@@ -12,4 +21,14 @@ contract HashMaps{
     function AddNames(uint _id,string memory _str) public{
         names[_id]=_str;
     }
+
+    // Let's add some books
+    // book --> title , author
+
+   // function to add a book with its id , title and author in the Books hashmap
+    function AddBooks(uint _id, string memory _title, string memory _author) public {
+        Books[_id]=Book(_title,_author);
+    }
+
+
 }
